8936183bb6dac9c605e3c6ef97d470fc96ac5319
henriquevelardo <henriquevelardo@hotmail.com>
2017-05-05 11:51:48 -0300

    Adicionando os métodos não implementados classe Usuario (Adm e comum)

--- Código/src/ADM.java
+++ Código/src/ADM.java
@@ -1,4 +1,12 @@
+//Os métodos não estão implementados ainda.
 
 public class ADM extends Usuario {
+     
+    public String criarLeilão() {
+    return "Aberto";   
+    }
     
+    public String adicionarParticipante() {
+    return "Jogador adicionado ao leilão";
+    }
 }
--- Código/src/Comum.java
+++ Código/src/Comum.java
@@ -1,4 +1,9 @@
+//Método p/ alterações conforme o desenvolvimento.
 
 public class Comum extends Usuario {
     
+    public String darLance() {
+    double lance = 0;
+    return "Valor do lance : " +lance;
+    }
 }
