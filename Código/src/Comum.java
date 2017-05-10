46cefb4408d7de1c6f438249393c8a5212a7c87c
henriquevelardo <henriquevelardo@hotmail.com>
2017-05-10 10:05:40 -0300

    Atualização da minha branch

--- Código/src/Comum.java
+++ Código/src/Comum.java
@@ -1,8 +1,12 @@
-//Método p/ alterações conforme o desenvolvimento. --branch
+//Método p/ alterações conforme o desenvolvimento. --branchhick
 
 public class Comum extends Usuario {
     
   public String Permissão() {
-  return "Permissão";
+  return "Permissão para entrar no leilão";
+  
+  
   }
+  
+  
 }
