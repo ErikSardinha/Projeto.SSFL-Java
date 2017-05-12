607d2495742f2cdbe23bedd636e166849f4ff02d
henriquevelardo <henriquevelardo@hotmail.com>
2017-05-12 11:58:52 -0300

    Arquivo permissões(1.2)

--- Código/src/Comum.java
+++ Código/src/Comum.java
@@ -1,25 +1,18 @@
 
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
 import javax.swing.JOptionPane;
+
+
 
 //Método p/ alterações conforme o desenvolvimento. --branchhick
 
 public class Comum extends Usuario {
     
-
-   
     public String permissao() {
     String p = JOptionPane.showInputDialog(null,"Digite o nome do leilão:");
     return "Pedindo permissão ao ADM";
     }
     
-   public void escrever() throws IOException {
-       Scanner ler = new Scanner("permissao.txt");
-    } 
+    
     
   }
   
