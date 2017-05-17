2439c2d2477539a4d3670d06f2b82bc23e8a5f95
BennyWahba <BennyWahba@github.com>
2017-05-17 10:01:38 -0300

    att

--- Código/src/Usuario.java
+++ Código/src/Usuario.java
@@ -1,13 +1,19 @@
-
 import javax.swing.JOptionPane;
 
 
-    abstract class  Usuario {
+    public class Usuario {
     private String nome;
     private String nickname;
     private String email;
-    private double loteria;
+    private double carteira;
     private Elenco elenco;
+    
+    public Usuario(String nome, String nickname, String email){
+        this.nome = nome;
+        this.nickname = nickname;
+        this.email = email;
+    
+    }
     
     public String darLance() {
     double lance = 0;
