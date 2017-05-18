61d06568368fb5ead8150d4b6680c526ca55befc
BennyWahba <benny.bw@hotmail.com>
2017-05-18 11:56:59 -0300

    Controlador de arquivo

--- Código/src/Arquivo.java
+++ Código/src/Arquivo.java
@@ -1,22 +1,34 @@
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class Arquivo {
-Scanner scanner = new Scanner(new FileReader("arquivo.txt"))                       .useDelimiter("\\||\\n");
-while (scanner.hasNext()) {
-  String nome = scanner.next();
-  String cidade = scanner.next();
-  String compras = scanner.next();
-  System.out.println(nome);
-  System.out.println(cidade);
-  System.out.println(compras);
-}
+    
+    public Arquivo(){};
+    
+    public String localArquivo(){
+    
         String path = new File("").getAbsolutePath();
-        FileInputStream arq; //= arquivo para leitura
-        FileOutputStream arq2; //= arquivo para escrita
-
-        while (true) { //= loop infinito. só sai com break ou return
+        return(path);
+    
+    }
+    public int tamanhoArquivo(String arq) throws IOException{
+    
+        int size = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(arq))) {
+            while(br.ready()){
+                String linha = br.readLine();
+                size = size + 1;
+                
+            }
+        } 
+        return (size);
+          
+    }
 }
+
